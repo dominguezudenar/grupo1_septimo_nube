7bd8e8ad8ae0759fa0cdccf2e66be786b1cbed46
camilodel <camiloargoty@gmail.com>
2016-10-05 01:07:51 -0500

    Ejercicio3: Se crea la estructura, colores y recursos multimedia

---
+++ modulo1/DelgadoGuerrero/Ejercicio3/Script.js
@@ -0,0 +1,18 @@
+function cambiarColor1 (){
+		document.getElementById('g').style.backgroundColor = "Purple";
+}
+function cambiarColor2 (){
+		document.getElementById('j').style.backgroundColor = "red";
+}
+function cambiarColor3 (){
+		document.getElementById('h').style.backgroundColor = "yellow";
+}
+function cambiarColor4 (){
+		document.getElementById('block1').style.backgroundColor = "RoyalBlue";
+}
+function cambiarColor5 (){
+		document.getElementById('i').style.backgroundColor = "pink";
+}
+function cambiarColor6 (){
+		document.getElementById('b').style.backgroundColor = "green";
+}
